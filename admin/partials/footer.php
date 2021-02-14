f911889c3c27f8d44f861dad5b5bf87d0872ce74
John Kevin <johnkevindejesus01@gmail.com>
2021-02-15 01:03:35 +0800

    second edit

---
+++ admin/partials/footer.php
@@ -0,0 +1,11 @@
+  <!-- Footer Section Starts -->
+  <div class="admin-footer">
+    <div class="wrapper">
+      <p class="text-center">2021 All rights reserved, Some Restaurant Developed By - <a href='#'>John Kevin</a></p>
+    </div>
+  </div> 
+  <!-- Footer Section Ends --> 
+
+
+</body>
+</html>
