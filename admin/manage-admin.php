86fd6b0fd72ba1940a32e00088ba5b32904b55da
John Kevin <johnkevindejesus01@gmail.com>
2021-02-15 01:24:57 +0800

    editing the style.css and admin.css

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,7 +5,24 @@
   <div class="main-content">
     <div class="wrapper">
       <h1>Manage Admin</h1>
+      <table>
+        <tr>
+          <th>S.N</th>
+          <th>Full Name</th>
+          <th>Username</th>
+          <th>Action</th>
+        </tr>
 
+        <tr>
+          <td>1.</td>
+          <td>John Kevin</td>
+          <td>kevsss</td>
+          <td>
+              Update Admin
+              Delete Admin
+          </td>
+        </tr>
+      </table>
     </div>
   </div>
   <!-- Main Content Section Ends --> 
--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,5 +1,5 @@
   <!-- Footer Section Starts -->
-  <div class="admin-footer">
+  <div class="footer">
     <div class="wrapper">
       <p class="text-center">2021 All rights reserved, Some Restaurant Developed By - <a href='#'>John Kevin</a></p>
     </div>
