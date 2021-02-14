ef8f7e53ae50c09752eeab91be58fe664b376879
John Kevin <johnkevindejesus01@gmail.com>
2021-02-15 03:29:41 +0800

    third edit

---
+++ admin/add-admin.php
@@ -0,0 +1,87 @@
+<?php include('partials/menu.php')?>
+
+
+<div class="main-content">
+  <div class="wrapper">
+    <h1>Add Admin</h1>
+
+    <br><br>
+
+    <?php
+      //checking the session is set or not
+      if(isset($_SESSION['add'])){ 
+        echo $_SESSION['add']; //displaying session massage if set
+        unset($_SESSION['add']); //removing session message
+      }
+    ?>
+
+    <br><br><br>
+
+    <form action="" method="POST">
+      <table class="tbl-30">
+        <tr>
+          <td>Full Name: </td>
+          <td><input type="text" name="full_name" placeholder="Enter Your Name" required></td>
+        </tr>
+
+        <tr>
+          <td>Username: </td>
+          <td><input type="text" name="username" placeholder="Your Username" required></td>
+        </tr>
+
+        <tr>
+          <td>Password: </td>
+          <td><input type="password" name="password" placeholder="Your Password" required></td>
+        </tr>
+
+        <tr>
+          <td colspan="2"><input type="submit" name="submit" value="Add Admin" class="btn-secondary"></td>
+        </tr>
+      </table>
+    </form>
+  </div>
+</div>
+
+<?php include('partials/footer.php')?>
+
+
+<?php
+    //Process the value from Form and Save it in Database
+    //Check Whether the submit button is clicked or not
+
+
+    // button clicked
+    if(isset($_POST['submit'])){
+      
+      // Get the Data Value from Form
+      $full_name = $_POST['full_name'];
+      $username = $_POST['username'];
+      $password = md5($_POST['password']); // password enctription with MD5
+      
+
+      // SQL Query to save the data to database
+      $sql = "INSERT INTO tbl_admin SET
+          full_name = '$full_name',
+          username = '$username',
+          password = '$password'
+      ";
+
+      // executing query and saving data in database
+      $res = mysqli_query($conn, $sql) or die(mysqli_error());
+
+      //check whether the (query is executed) data is inserted or not and display appropriate message
+      if($res==TRUE) {
+        //create session variable to display message
+        $_SESSION['add'] = "Admin Added Successfully";
+        //redirect page to manage admin
+        header("location:".SITEURL.'admin/manage-admin.php');
+      }
+      else {
+        //create session variable to display message
+        $_SESSION['add'] = "Failed to Add Admin";
+        //redirect page to add admin
+        header("location:".SITEURL.'admin/add-admin.php');
+      }
+    }
+   
+?>
--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,7 +5,26 @@
   <div class="main-content">
     <div class="wrapper">
       <h1>Manage Admin</h1>
-      <table>
+
+      <br><br>
+      
+      <?php
+        //checking the session is set or not
+        if(isset($_SESSION['add'])){
+          echo $_SESSION['add']; //displaying session massage if set
+          unset($_SESSION['add']); //removing session message
+        }
+      ?>
+
+      <br><br><br>
+
+
+      <!--  Button to Add Admin  -->
+      <a href="add-admin.php" class="btn-primary">Add Admin</a>
+
+      <br><br>
+
+      <table class="tbl-full">
         <tr>
           <th>S.N</th>
           <th>Full Name</th>
@@ -13,16 +32,53 @@
           <th>Action</th>
         </tr>
 
-        <tr>
-          <td>1.</td>
-          <td>John Kevin</td>
-          <td>kevsss</td>
-          <td>
-              Update Admin
-              Delete Admin
-          </td>
-        </tr>
+        <?php
+          //query to get all admin
+          $sql = "SELECT * FROM tbl_admin";
+          //execute the query
+          $res = mysqli_query($conn, $sql);
+
+          //check whether the query is executed or not
+          if($res==TRUE) {
+            //count rows to check whether we have data in database or not
+            $rows = mysqli_num_rows($res); // function to get all the rows in database
+
+            $sn=1; //create a variable and assign value
+
+            //check the number of rows
+            if($rows>0){
+              while($rows=mysqli_fetch_assoc($res)){
+                //using the while loop to get all the data from database
+                //and while loop will run as long as we have data in database
+
+                //get individual data
+                $id=$rows['id'];
+                $full_name=$rows['full_name'];
+                $username=$rows['username'];
+
+                //display the values in our table
+        ?>
+
+                <tr>
+                  <td><?php echo $sn++?></td>
+                  <td><?php echo $full_name?></td>
+                  <td><?php echo $username?></td>
+                  <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                  </td>
+                </tr>
+
+        <?php
+              }
+            }
+            else{
+              // we do not have data in database
+            }
+          }
+        ?>
       </table>
+
     </div>
   </div>
   <!-- Main Content Section Ends --> 
